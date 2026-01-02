5fa2e3820931f012e2b9dcb02920284e83752795
Cledilson Filho <cledilsondevoffice@gmail.com>
2026-01-02 12:16:18 -0300

    Criação das classe Chamado

--- src/main/java/com/cledilsondevoffice/helpdesk/domain/Chamado.java
+++ src/main/java/com/cledilsondevoffice/helpdesk/domain/Chamado.java
@@ -1,4 +1,121 @@
 package com.cledilsondevoffice.helpdesk.domain;
 
+import com.cledilsondevoffice.helpdesk.domain.enuns.Prioridade;
+import com.cledilsondevoffice.helpdesk.domain.enuns.Status;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Chamado {
+
+    private Integer id;
+    private LocalDate dataAbertura = LocalDate.now();
+    private LocalDate dataFechamento;
+    private Status status;
+    private Prioridade prioridade;
+    private String titulo;
+    private String observacoes;
+
+    private Tecnico tecnico;
+    private Cliente cliente;
+
+    public Chamado() {
+        super();
+    }
+
+    public Chamado(Integer id, Status status, Prioridade prioridade, String observacoes, String titulo, Tecnico tecnico, Cliente cliente) {
+        this.id = id;
+        this.status = status;
+        this.prioridade = prioridade;
+        this.observacoes = observacoes;
+        this.titulo = titulo;
+        this.tecnico = tecnico;
+        this.cliente = cliente;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDate dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public LocalDate getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(LocalDate dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Prioridade getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Prioridade prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public Tecnico getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(Tecnico tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Chamado chamado = (Chamado) o;
+        return Objects.equals(id, chamado.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
+
+
